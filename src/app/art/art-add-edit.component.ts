9f523c1d62250d7b99f866da9912ee66514f607f
Natasha Sukhinina <natashas@openkey.io>
2018-02-23 20:29:38 -0600

    Remove selected art tags from suggestions

--- src/app/art/art-add-edit.component.ts
+++ src/app/art/art-add-edit.component.ts
@@ -17,35 +17,15 @@ export class ArtAddEditComponent implements OnInit {
 	public artForm: FormGroup;
 	public artTagsForm: FormArray;
 
+	public image_url;
+	private art_url;
 	public artTags;
+
 	artTagsSuggestions: any[];
 
 	constructor(private formBuilder: FormBuilder, 
 		public activeModal: NgbActiveModal,
 		private artService: ArtService) {}
-
-	suggestArtTags(event) {
-
-		let query = event.query;
-
-		this.artService.getArtTags().then(
-			artTags => {
-				this.artTagsSuggestions = this.filterArtTags(query, artTags);
-				console.log(this.artTagsSuggestions)
-			});
-	}
-
-	filterArtTags(query, artTags: any[]):any[] {
-		let filtered : any[] = [];
-		for(let i = 0; i < artTags.length; i++) {
-			let artTag = artTags[i];
-			if(artTag.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-				filtered.push(artTag);
-			}
-		}
-		return filtered;
-	}
-
 
 	ngOnInit() {
 
@@ -57,28 +37,55 @@ export class ArtAddEditComponent implements OnInit {
 			'art-file': ['', Validators.required],
 		})
 
+		this.artTagsForm = this.formBuilder.array([])
+
 	}
+
+	suggestArtTags(event) {
+
+		let query = event.query;
+
+		this.artService.getArtTags().then(
+			artTags => {
+				this.artTagsSuggestions = this.filterArtTags(query, artTags);
+			});
+	}
+
+	filterArtTags(query, artTags: any[]):any[] {
+		let filtered : any[] = [];
+		for(let i = 0; i < artTags.length; i++) {
+			let artTag = artTags[i];
+			if(artTag.name.toLowerCase().indexOf(query.toLowerCase()) == 0 && this.artTagsForm.value.indexOf(artTag.id) == -1) {
+				filtered.push(artTag);
+			}
+		}
+		return filtered;
+	}
+
+	onTagSelect(tag) {
+		console.log(tag)
+		this.artTagsForm.push(new FormControl(tag.id))
+
+		console.log(this.artTagsForm.value)
+	}
+
+
 
 	onFileUpload(event) {
 
 		if(event.target.files && event.target.files.length > 0) {
-			let file = event.target.files[0];
 
-			let storageRef = firebase.storage().ref('arts/' + file.name);
+			this.art_url = event.target.files[0];
 
-			storageRef.put(file).on('state_changed',
-				(snapshot) => {
-				},
-				(error) => {
-					console.log(error)
-				},
-				() => {
-					storageRef.getDownloadURL().then(
-						(url) => {
-							this.artForm.get('art-file').setValue(url);
-							console.log(this.artForm.value)
-						})
-				})
+			var reader = new FileReader();
+
+			reader.onload = (event:any) => {
+				this.image_url = event.target.result;
+			}
+
+			reader.readAsDataURL(event.target.files[0]);
+
+
 
 		}
 	}
@@ -88,6 +95,25 @@ export class ArtAddEditComponent implements OnInit {
 	}
 
 	onSubmit() {
-		console.log(this.artForm);
+		console.log(this.artForm.get('art-tags').value);
+
+		// let storageRef = firebase.storage().ref('arts/' + this.art_url.name);
+
+		// storageRef.put(this.art_url).on('state_changed',
+		// 	(snapshot) => {
+		// 	},
+		// 	(error) => {
+		// 		console.log(error)
+		// 	},
+		// 	() => {
+		// 		storageRef.getDownloadURL().then(
+		// 			(url) => {
+		// 				this.artForm.get('art-file').setValue(url);
+		// 				console.log(this.artForm.value)
+		// 			})
+		// 	})
+
+		// this.image_url = '';
+
 	}
 }
--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -12,13 +12,6 @@ export class ArtComponent {
 
 	date = new Date;
 
-	// arts: Art[] = [
-	// 	new Art('Test Image', 'Test Description', 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%287%29.jpg', 'today', ['paint', 'nature']),
-	// 	new Art('Test Image', 'Test Description', 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%287%29.jpg', 'today', ['pencil', 'contest']),
-	// 	new Art('Test Image', 'Test Description', 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%287%29.jpg', 'today', ['video', 'nature']),
-	// 	new Art('Test Image', 'Test Description', 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%287%29.jpg', 'today', ['paint', 'contest', 'blahblah'])
-	// ];
-
 	arts = [
 		{
 			'id': 1,
